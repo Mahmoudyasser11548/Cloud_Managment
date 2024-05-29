2cf7baa4e5b9d4921acb3cb76ed701547695d2f8
mmmragab <m.m.ragab@hotmail.com>
2024-05-29 13:51:24 +0300

    adjustment

--- src/views/Error.tsx
+++ src/views/Error.tsx
@@ -7,7 +7,7 @@ const Error = () => {
       <div className="flex h-screen items-center justify-center ">
           <div className=" ms-12">
             <h1 className="font-bold" >404: Not Found</h1>
-            <p>We couldn't find that page, please check the URL and try again.</p>
+            <p>We couldn't find that page, please check the URL and try again.mm</p>
             <a href="/home" className='underline text-blue-600'> Go to Home </a>
           </div>
         <img src='imgs/error.jpg' alt='Not Found' />
--- src/views/NotAuthorized.tsx
+++ src/views/NotAuthorized.tsx
@@ -7,7 +7,7 @@ const NotAuthorized = () => {
       <img src='imgs/notauth.jpg' alt='Not Found' />
       <div>
         <h1 className="font-bold " >403- Uh-oh, you do not have access.</h1>
-        <p className=" font-bold mb-3">Your account is not authorized to view this page.</p>
+        <p className=" font-bold mb-3">Your account is not authorized to view this page.mm</p>
         <a href="/home" className='underline text-blue-600'> Go to Home </a>
       </div>
     </div>
